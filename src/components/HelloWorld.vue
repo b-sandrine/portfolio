b5921ccf3f7bcb08266ab55349d4a7296974f6eb
Dushimimana Sandrine <sanrdushimimana@gmail.com>
2026-04-22 09:22:06 +0200

    Built the project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    msg: string
+}>()
+</script>
+
+<style scoped>
+h1 {
+    font-weight: 600;
+}
+</style>
